0fedc63d8e1a2020fef4377569ee8b57627a40b1
Vadim130 <vadim.uskov.2003@mail.ru>
2024-02-29 12:00:09 +0200

    +2.1.1, +2.1.2 (исправленные для совместимости со старым .Net)

---
+++ 2.1.1/2.1.1.cs
@@ -0,0 +1,21 @@
+﻿public class Task211
+{ 
+    static async Task<T> DelayResult<T>(T result, TimeSpan delay)
+    {
+        await Task.Delay(delay);
+        return result;
+    }
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Задача 2.1.1");
+        Task<int> t = DelayResult<int>(10, TimeSpan.FromSeconds(3));
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(100);
+            Console.Write(i);
+        }
+        Console.WriteLine();
+        int v = await t;
+        Console.WriteLine("Value={0}", v);
+    }
+}
---
+++ 2.1.2/2.1.2.cs
@@ -0,0 +1,39 @@
+﻿public class Task212
+{ 
+    static async Task<string> DownloadStringWithRetries(HttpClient client, string uri)
+    {
+        // Повторить попытку через 1 секунду, потом через 2 и через 4 секунды.
+        TimeSpan nextDelay = TimeSpan.FromSeconds(1);
+        for (int i = 0; i != 3; ++i)
+        {
+            try
+            {
+                return await client.GetStringAsync(uri);
+            }
+            catch
+            {
+            }
+            await Task.Delay(nextDelay);
+            nextDelay = nextDelay + nextDelay;
+        }
+        // Попробовать в последний раз и разрешить распространение ошибки.
+        return await client.GetStringAsync(uri);
+    }
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Задача 2.1.2");
+        HttpClient c = new HttpClient();
+        string uri = "http://www.mail.ru";
+        Task<string> t = DownloadStringWithRetries(c, uri);
+        Console.WriteLine("Downloading {0}...", uri);
+        try
+        {
+            string s = await t;
+            Console.WriteLine("Got message:{0}", s);
+        }
+        catch (System.Net.Http.HttpRequestException x)
+        {
+            Console.WriteLine("Exception occured, details: {0}", x.ToString());
+        }
+    }
+}
