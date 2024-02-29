ae931b7bb118c1d8e4b3a2e082fbcb0f8132aef5
Vadim130 <vadim.uskov.2003@mail.ru>
2024-02-29 12:15:00 +0200

    +2.4.1

---
+++ 2.2.4/2.4.1.cs
@@ -0,0 +1,19 @@
+﻿public class Task224
+{
+    public static async Task Task1()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+    public static async Task Task2()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(2));
+    }
+
+    public static async Task Main(string[] args)
+    {
+        Task task1 = Task1();
+        Task task2 = Task2();
+        Task task3 = Task.Delay(TimeSpan.FromSeconds(1));
+        await Task.WhenAll(task1, task2, task3);
+    }
+}
