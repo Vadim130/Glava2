15bd5c1e6e3358fa25bd7fc8c0650b56af0cdf73
Vadim130 <vadim.uskov.2003@mail.ru>
2024-03-02 12:24:11 +0200

    2.4.2, 2.4.3

---
+++ 2.4.3/2.4.3.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+public class Task243
+{
+
+    public static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        string[] urls = { "http://mail.ru", "https://google.com", "https://ya.ru" };
+        Task<string> t = DownloadAllAsync(client, urls);
+        Console.WriteLine("Скачано: {0}", await t);
+    }
+    static async Task<string> DownloadAllAsync(HttpClient client,
+     IEnumerable<string> urls)
+    {
+        // Определить действие, выполняемое для каждого URL.
+        var downloads = urls.Select(url => client.GetStringAsync(url));
+        // Обратите внимание: задачи еще не запущены,
+        // потому что последовательность не была обработана.
+        // Запустить загрузку для всех URL одновременно.
+        Task<string>[] downloadTasks = downloads.ToArray();
+        // Все задачи запущены.
+        // Асинхронно ожидать завершения всех загрузок.
+        string[] htmlPages = await Task.WhenAll(downloadTasks);
+        return string.Concat(htmlPages);
+    }
+}
