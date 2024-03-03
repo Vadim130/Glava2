2e627ed940712f8990121bb66908a6f3796b71c9
Vadim130 <vadim.uskov.2003@mail.ru>
2024-03-03 16:58:46 +0200

    2.5 и 2.6.1

---
+++ 2.5/2.5.cs
@@ -0,0 +1,25 @@
+﻿public class Task25
+{
+
+    public static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        Task<int> t = FirstRespondingUrlAsync(client, "http://mail.ru", "https://ya.ru");
+        Console.WriteLine("Длина данных: {0}", await t);
+
+    }
+    // Возвращает длину данных первого ответившего URL-адреса.
+    static async Task<int> FirstRespondingUrlAsync(HttpClient client,
+     string urlA, string urlB)
+    {
+        // Запустить обе загрузки параллельно.
+        Task<byte[]> downloadTaskA = client.GetByteArrayAsync(urlA);
+        Task<byte[]> downloadTaskB = client.GetByteArrayAsync(urlB);
+        // Ожидать завершения любой из этих задач.
+        Task<byte[]> completedTask =
+        await Task.WhenAny(downloadTaskA, downloadTaskB);
+        // Вернуть длину данных, загруженных по этому URL-адресу.
+        byte[] data = await completedTask;
+        return data.Length;
+    }
+}
---
+++ 2.6.1/2.6.1.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+public class Task261
+{
+    public static async Task Main(string[] args)
+    {
+        Task t = ProcessTasksAsync();
+        await t;
+    }
+
+    static async Task<int> DelayAndReturnAsync(int value)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(value));
+        return value;
+    }
+    // В текущей версии метод выводит "2", "3" и "1".
+    // При этом метод должен выводить "1", "2" и "3".
+    static async Task ProcessTasksAsync()
+    {
+        // Создать последовательность задач.
+        Task<int> taskA = DelayAndReturnAsync(2);
+        Task<int> taskB = DelayAndReturnAsync(3);
+        Task<int> taskC = DelayAndReturnAsync(1);
+        Task<int>[] tasks = new[] { taskA, taskB, taskC };
+        // Ожидать каждую задачу по порядку.
+        foreach (Task<int> task in tasks)
+        {
+            var result = await task;
+            Trace.WriteLine(result);
+        }
+    }
+}
